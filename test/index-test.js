573723c75fbcd171f059c10941ce6db046c02582
Susan <suzutoyuki@gmail.com>
2018-01-18 07:32:28 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -16,7 +16,7 @@ describe('what-is-a-test', () => {
   })
 
   describe('Message', () => {
-    it('gives the name and height', () => {
+    it('gives the names and heights', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
